aafbda5a3b279443946257ba8df609f05ebd26da
osshiya <osshiya@gmail.com>
2021-05-07 15:48:40 +0800

    added portfolio page

--- POP/src/app/app-routing.module.ts
+++ POP/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'portfolio',
+    loadChildren: () => import('./portfolio/portfolio.module').then( m => m.PortfolioPageModule)
+  },
 ];
 
 @NgModule({

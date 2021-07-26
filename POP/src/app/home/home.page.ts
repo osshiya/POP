be775d7360d3e8e99bbb1499f66cbd58a52911aa
osshiya <osshiya@gmail.com>
2021-07-26 14:55:42 +0800

    new ui for menu and tab

--- POP/src/app/home/home.page.ts
+++ POP/src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
+import { DataService } from '../services/data.service';
+import { Storage } from '@ionic/storage';
 import { DiscoverPage } from './discover/discover.page';
 
 @Component({
@@ -8,8 +12,69 @@ import { DiscoverPage } from './discover/discover.page';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private menu: MenuController,
+    private dataService: DataService,
+  ) { }
+
+  currentsid: any;
+  userinfos: any = [];
 
   ngOnInit() {
+    this.myData();
   }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  gotoChat() {
+    this.router.navigate(['chat'])
+    console.log("gotoChatclicked");
+  }
+
+  gotoAct() {
+    this.router.navigate(['activity'])
+    console.log("gotoActivityclicked");
+  }
+
+
+  async myData(){
+    const storage = new Storage();
+    await storage.create();
+    this.currentsid = await storage.get('usersid');
+
+    this.retrieveUser();
+    // this.retrieveUserPortfolio(currentsid);
+  }
+
+  retrieveUser(){
+    // console.log("retrieve Discover");
+    this.dataService.getProfile().subscribe(response => {
+      if(response != null){  
+      //this.showToast('Logged in');
+        // console.log('link:' + 'https://student.amphibistudio.sg/10187403A/POP/db/posts.php?x=');
+        // console.log(response);
+        this.userinfos = response;
+        console.log(this.userinfos);
+      }else{
+        //this.showErrorToast('Wrong userid/ password');
+      }
+  })
+}
+
+async logout(){
+  console.log("logging out");
+
+  const storage = new Storage();
+  await storage.create();
+
+  await storage.set('usersid', '');
+  await storage.set('userpassword', '');
+
+  this.router.navigate(['/login']);
+}
+
 }
